ef7593eff5aa28cedfd5a9b90b5354ce531cc9a8
Seth Thomas <sthomas@chef.io>
2015-10-26 12:39:10 -0700

    Cleaning up namespaces and provisioner logic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,15 @@
 default['chef_portal']['user'] = 'root' # sudo picks up ec2-metadata
 default['chef_portal']['password'] = 'chefportal'
+default['chef_portal']['root_dir'] = '/root/portal_site'
 
-default['chef_portal']['versions']['chef_dk'] = '0.6.2'
-default['chef_portal']['versions']['chef_provisioning'] = '1.2.1'
-default['chef_portal']['versions']['chef_provisioning_aws'] = '1.3.0'
+default['chef_portal']['chefdk']['version'] = '0.8.1'
+
+default['chef_portal']['chefdk']['gems'].tap do |gems|
+  gems['chef-provisioning-aws'] = '1.5.1'
+end
+
+default['chef_portal']['pkgs'] = %w(git)
+
+default['chef_portal']['chef_classroom']['repo'] = 'git://github.com/chef-training/chef_classroom.git'
+default['chef_portal']['chef_classroom']['branch'] = 'master'
+default['chef_portal']['chef_classroom']['dir'] = '/root/chef_classroom'
--- recipes/provisioner_node.rb
+++ recipes/provisioner_node.rb
@@ -1,34 +1,9 @@
-#
 # Cookbook Name:: chef_portal
 # Recipe:: provisioner_node
-#
-# Author:: George Miranda (<gmiranda@chef.io>)
-# License:: MIT
-# Copyright (c) 2015 Chef Software, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
 portal_user = node['chef_portal']['user']
 portal_pass = node['chef_portal']['password'].crypt('$6$' + rand(36**8).to_s(36))
-chefdk_ver = node['chef_portal']['versions']['chef_dk']
-provis_ver = node['chef_portal']['versions']['chef_provisioning']
-cp_aws_ver = node['chef_portal']['versions']['chef_provisioning_aws']
+chefdk_ver = node['chef_portal']['chefdk']['version']
 
 # Setup the portal user
 user portal_user do
@@ -68,29 +43,17 @@ template '/root/.bashrc' do
   mode '0640'
 end
 
-# Explicitly pin chef-provisioning
-# Explicitly pin chef-provisioning-aws
-# (shell init may not be yet available, so we have to fake it)
-execute 'update_chef_provisioning' do
-  command "chef gem install chef-provisioning -v #{provis_ver}"
-  environment(
-    'PATH' => '/opt/chefdk/bin:/opt/chefdk/embedded/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
-    'GEM_ROOT' => '/opt/chefdk/embedded/lib/ruby/gems/2.1.0',
-    'GEM_HOME' => '/root/.chefdk/gem/ruby/2.1.0',
-    'GEM_PATH' => '/root/.chefdk/gem/ruby/2.1.0:/opt/chefdk/embedded/lib/ruby/gems/2.1.0'
-  )
-  # not_if 'gem update is idempotent enough for now'
+# install packages
+package node['chef_portal']['pkgs'] do
+  action :install
 end
 
-execute 'update_chef_provisioning' do
-  command "chef gem install chef-provisioning-aws -v #{cp_aws_ver}"
-  environment(
-    'PATH' => '/opt/chefdk/bin:/opt/chefdk/embedded/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
-    'GEM_ROOT' => '/opt/chefdk/embedded/lib/ruby/gems/2.1.0',
-    'GEM_HOME' => '/root/.chefdk/gem/ruby/2.1.0',
-    'GEM_PATH' => '/root/.chefdk/gem/ruby/2.1.0:/opt/chefdk/embedded/lib/ruby/gems/2.1.0'
-  )
-  # not_if 'gem update is idempotent enough for now'
+# install gems if present
+node['chef_portal']['chefdk']['gems'].each do |gem, ver|
+  gem_package gem do
+    gem_binary '/opt/chefdk/embedded/bin/gem'
+    version ver
+  end
 end
 
 # get my AWS creds from IAM
