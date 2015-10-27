3118ca4b379befc11007e4abc132ec859cb27129
Seth Thomas <sthomas@chef.io>
2015-10-27 09:32:48 -0700

    Reverting changes here because I think it broke things even if it made rubocop happy

--- recipes/_refresh_nodes.rb
+++ recipes/_refresh_nodes.rb
@@ -1,14 +1,7 @@
 # Cookbook Name:: chef_portal
 # Recipe:: _refesh_iam_creds
 
-current_node_export = {
-  :class_name => node['chef_classroom']['class_name'],
-  :console_address => "http://#{node['ec2']['public_ipv4']}:8080/guacamole",
-  :key => "/root/.ssh/#{node['chef_classroom']['class_name']}-portal_key",
-  :workstations => workstation_nodes,
-  :nodes => additional_nodes,
-  :chefserver => chefserver_nodes
-}
+current_node_export = node_export
 
 template "#{node['chef_portal']['root_dir']}/nodes.yml" do
   source 'webapp/nodes.yml.erb'
