637dfb710df608afa9cd4deb7f2c175457e3bc27
Seth Thomas <sthomas@chef.io>
2015-10-30 14:22:36 -0700

    Moving hash back into library where it belongs

--- recipes/_refresh_nodes.rb
+++ recipes/_refresh_nodes.rb
@@ -1,14 +1,6 @@
 # Cookbook Name:: chef_portal
 # Recipe:: _refesh_iam_creds
 
-current_node_export = {
-  :class_name => node['chef_classroom']['class_name'],
-  :console_address => "http://#{node['ec2']['public_ipv4']}:8080/guacamole",
-  :key => "/root/.ssh/#{node['chef_classroom']['class_name']}-portal_key",
-  :students => find_students,
-  :chefserver => find_chefserver
-}
-
 template "#{node['chef_portal']['root_dir']}/nodes.yml" do
   source 'webapp/nodes.yml.erb'
   mode '0644'
